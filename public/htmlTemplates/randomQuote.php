4e82f1b27b2f5f4f983b576ae5eecabfa7e66fc7
Attila Lajko <lajkoatis@gmail.com>
2024-10-18 17:24:14 +0200

    nameChange

--- public/htmlTemplates/randomQuote.php
+++ public/htmlTemplates/randomQuote.php
@@ -1,5 +1,5 @@
 <?php
-$file = 'C:/xampp/htdocs/LA_Bank SPA óvatos/src/txt/quotes.txt';
+$file = 'C:/xampp/htdocs/LA_Bank SPA/src/txt/quotes.txt';
 $quotes = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 $randomIndex = array_rand($quotes);
